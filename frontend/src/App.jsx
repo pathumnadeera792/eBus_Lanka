b1384d9dfd61d4aa4c2da563fc0061fbdcec0204
pathumnadeera720 <pathumnadeera100@gmail.com>
2026-08-22 13:26:44 +0530

    Setup tailwind CSS ✏️

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div>
-      <h1>eBus Lanka Frontend is working!</h1>
+    <div className = " w-full h-screen bg-blue-300">
+      <div className = "w-[600px] h-[600px] bg-black flex flex-col justify-evenly items-center ">
+      <div className = "w-[75px] h-[75px] bg-yellow-600"></div>
+      <div className = "w-[75px] h-[75px] bg-red-600"></div>
+      <div className = "w-[75px] h-[75px] bg-green-600"></div>
+      <div className = "w-[75px] h-[75px] bg-pink-600"></div>
+      <div className = "w-[75px] h-[75px] bg-orange-600"></div>
+      <div className = "w-[75px] h-[75px] bg-indigo-600"></div>
+      </div>
     </div>
   )
 }
